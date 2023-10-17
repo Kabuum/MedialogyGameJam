b9646dd80e48765e5f4bc275080f64b4e7b8b960
Dog1144MED <mhoue23@student.aau.dk>
2023-10-17 13:29:25 +0200

    sdasdasd
    asd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject mainPlayer;
+    public GameObject lion;
+    public GameObject effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effect.SetActive(false);
+        lion.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerTransformation(0);
+        }
+    }
+
+    void PlayerTransformation(int playerStateIndex)
+    {
+
+        if (mainPlayer.activeSelf == true)
+        {
+            mainPlayer.SetActive(false);
+            lion.SetActive(true);
+        }
+        else if (mainPlayer.activeSelf == false)
+        {
+            lion.SetActive(false);
+            mainPlayer.SetActive(true);
+        }
+        effect.SetActive(true);
+        effect.GetComponent<Animator>().SetTrigger("Active");
+    }
+
+    public static void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+}
