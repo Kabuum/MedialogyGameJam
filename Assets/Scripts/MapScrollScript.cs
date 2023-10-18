7424e46856db58ebff99f4dee9b57ccece2b9d93
Martin Riber Thomsen <thomsenribermartin@gmail.com>
2023-10-18 12:17:08 +0200

    Fix map Scroll In Scene Martin

--- Assets/Scripts/MapScrollScript.cs
+++ Assets/Scripts/MapScrollScript.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class MapScrollScript: MonoBehaviour
 {
-
     public float camSpeed;
     public Vector3 offests;
-
-    // Start is called before the first frame update
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-
-
         transform.position = transform.position + offests * camSpeed * Time.deltaTime;
     }
 }
