3c460f0b908a46f9d1aced36d46d6168475ef2b8
matt0694 <mathan23@student.aau.dk>
2023-10-18 13:04:56 +0200

    eagle script

---
+++ Assets/Scripts/PlayerEagle.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEagle : MonoBehaviour
+{
+    public Camera cam;
+    public float camX;
+
+    public float maxSpeed;
+    public Rigidbody2D rb;
+    public float flap;
+    public float moveSpeed;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        maxSpeed = 5f;
+        flap = 20f; 
+        moveSpeed = 10f;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up*flap,ForceMode2D.Impulse);
+            anim.Play("eagleAnimation");
+        }
+        else
+        {
+ 
+        }
+
+        if (rb.velocity.magnitude < maxSpeed*-1)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed*-1);
+        }
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector2.left*(moveSpeed*Time.deltaTime));
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector2.right*(moveSpeed*Time.deltaTime));
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
